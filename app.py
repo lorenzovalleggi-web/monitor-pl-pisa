57ac084bc68edd816088e97e757c6cdc675b6029
lorenzovalleggi-web <lorenzovalleggi@gmail.com>
2026-05-29 08:16:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -118,11 +118,6 @@ with c3:
 
 st.link_button("📩 Diventa Sponsor", "mailto:info.railflow@gmail.com?subject=Sponsor")
 st.markdown("---")
-st.write("### 🚊 STATO VARCHI")
+st.write("### 𚊊 STATO VARCHI")
 
-varchi = [{"nome": "San Giuliano Terme", "pisa": 0, "lucca": 4}, {"nome": "Via Ulisse Dini (Gello)", "pisa": 2, "lucca": 3}, {"nome": "Via di Gagno (Pisa)", "pisa": 5, "lucca": 2}, {"nome": "Via Ugo Rindi (Pisa)", "pisa": 7, "lucca": 0}]
-
-for i, pl in enumerate(varchi):
-    if i > 0: st.write("### :arrow_down:")
-    chiuso = False
-    info
+varchi =
