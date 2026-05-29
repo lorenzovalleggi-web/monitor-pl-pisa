d57af6e5a1519218b78b56c4936faf70cea3217a
lorenzovalleggi-web <lorenzovalleggi@gmail.com>
2026-05-29 08:15:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -120,18 +120,4 @@ st.link_button("📩 Diventa Sponsor", "mailto:info.railflow@gmail.com?subject=S
 st.markdown("---")
 st.write("### 🚊 STATO VARCHI")
 
-varchi = [
-    {"nome": "San Giuliano Terme", "pisa": 0, "lucca": 4},
-    {"nome": "Via Ulisse Dini (Gello)", "pisa": 2, "lucca": 3},
-    {"nome": "Via di Gagno (Pisa)", "pisa": 5, "lucca": 2},
-    {"nome": "Via Ugo Rindi (Pisa)", "pisa": 7, "lucca": 0}
-]
-
-for i, pl in enumerate(varchi):
-    if i > 0: st.write("### :arrow_down:")
-    chiuso = False
-    info_pl = "Strada libera"
-    
-    for tr in lista_treni:
-        m_p = tr["ora_p"] * 60 + tr["min_p"] + tr["ritardo"]
-        durata = 10 if (tr["ora_p"]
+varchi =
