588f5fdfb545cc9b5916daa536854f4ef58dab9a
lorenzovalleggi-web <lorenzovalleggi@gmail.com>
2026-06-16 09:25:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-File "/mount/src/monitor-pl-pisa/app.py", line 54
-      {"ora": 15, "min": 23, "dir": "LUCCA", "num": "18532"}, {"ora": 15, "min
-                                                                          ^
-SyntaxError: unterminated string literal (detected at line 54)
+File "/mount/src/monitor-pl-pisa/app.py", line 2
+        {"ora": 15, "min": 23, "dir": "LUCCA", "num": "18532"}, {"ora": 15, "min
+                                                                            ^
+SyntaxError: unterminated string literal (detected at line 2)
